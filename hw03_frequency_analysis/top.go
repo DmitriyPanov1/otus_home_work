89d15abee98e2f63a69c0d49e8294a7200aa28db
Dmitriy <panodmitrij@gmail.com>
2024-06-27 14:02:02 +0500

    refactoring to the code and tests

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -16,20 +16,20 @@ func Top10(s string) []string {
 		return nil
 	}
 
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c) && c != '-'
+	}
+
 	m := make(map[string]uint)
-	var word strings.Builder
 
-	for _, r := range s {
-		r = unicode.ToLower(r)
+	for _, word := range strings.FieldsFunc(s, f) {
+		word = strings.ToLower(word)
 
-		if unicode.IsSpace(r) || r == '.' {
-			word, m = addWordToMap(word, m)
-		} else {
-			word.WriteRune(r)
+		if word != "-" {
+			m[word]++
 		}
 	}
 
-	_, m = addWordToMap(word, m)
 	words := make([]Word, 0, len(m))
 
 	for k, v := range m {
@@ -44,18 +44,6 @@ func Top10(s string) []string {
 	}
 
 	return result
-}
-
-func addWordToMap(word strings.Builder, m map[string]uint) (strings.Builder, map[string]uint) {
-	if word.Len() > 0 {
-		w := word.String()
-		if w != "-" {
-			m[w]++
-		}
-		word.Reset()
-	}
-
-	return word, m
 }
 
 func sortWords(words []Word) []Word {
