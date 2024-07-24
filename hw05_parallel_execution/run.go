9fb7599ef1cb8a7b7638545a5bfa7f68dee5df32
Dmitriy <panodmitrij@gmail.com>
2024-07-24 13:03:26 +0300

    hw05 parallel execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,83 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	taskCh := make(chan Task, len(tasks))
+	errorCh := make(chan error, len(tasks))
+	doneCh := make(chan struct{})
+	var completedTasks int
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < n; i++ {
+		go func() {
+			select {
+			case <-doneCh:
+				close(errorCh)
+				return
+			default:
+				for task := range taskCh {
+					if task != nil {
+						err := task()
+						errorCh <- err
+						completedTasks++
+					}
+
+					if completedTasks >= len(tasks) {
+						close(errorCh)
+
+						return
+					}
+				}
+			}
+		}()
+	}
+
+	go func() {
+		defer func() {
+			close(taskCh)
+		}()
+		for _, task := range tasks {
+			select {
+			case taskCh <- task:
+			case <-doneCh:
+				return
+			}
+		}
+	}()
+
+	var errCount int
+
+	wg.Add(1)
+	go func() {
+		defer func() {
+			wg.Done()
+			close(doneCh)
+		}()
+
+		for err := range errorCh {
+			if err != nil {
+				errCount++
+
+				if errCount >= m {
+					return
+				}
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	if errCount >= m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,80 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("m is more than the count of errors in the tasks", func(t *testing.T) {
+		tests := []struct {
+			name           string
+			workers        int
+			maxErrorsCount int
+			errorsCount    int
+			tasksCount     int
+		}{
+			{
+				"m > errorsCount",
+				5,
+				5,
+				4,
+				7,
+			},
+			{
+				"m = errorsCount",
+				5,
+				5,
+				5,
+				7,
+			},
+			{
+				"m = 0",
+				5,
+				0,
+				5,
+				7,
+			},
+			{
+				"m = 0 and errorCount = 0",
+				5,
+				0,
+				0,
+				7,
+			},
+		}
+
+		for _, tc := range tests {
+			tasks := make([]Task, 0, tc.tasksCount)
+
+			var runTasksCount int32
+			var countErrors int
+
+			for i := 0; i < tc.tasksCount; i++ {
+				err := fmt.Errorf("error from task %d", i)
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+					atomic.AddInt32(&runTasksCount, 1)
+
+					if countErrors < tc.errorsCount {
+						countErrors++
+
+						return err
+					}
+
+					return nil
+				})
+			}
+
+			err := Run(tasks, tc.workers, tc.maxErrorsCount)
+
+			if tc.errorsCount >= tc.maxErrorsCount {
+				require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			if tc.errorsCount > 0 {
+				require.LessOrEqual(t, runTasksCount, int32(tc.workers+tc.maxErrorsCount), "extra tasks were started")
+			} else {
+				require.Equal(t, int(runTasksCount), tc.tasksCount)
+			}
+		}
+	})
 }
