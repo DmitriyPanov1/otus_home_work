a52ff3e24bfc02e96c1014a92f855bffba75fdaf
Dmitriy <panodmitrij@gmail.com>
2024-07-29 13:32:49 +0300

    hw06 pipeline execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	return pipeline(in, done, stages...)
+}
+
+func pipeline(in In, done In, stages ...Stage) Out {
+	for _, stage := range stages {
+		in = execute(in, done, stage)
+	}
+
+	return in
+}
+
+func execute(in In, done In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		stageOut := stage(in)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-stageOut:
+				if !ok {
+					return
+				}
+
+				out <- v
+			}
+		}
+	}()
+
+	return out
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,18 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("empty IN", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		close(in)
+
+		result := make([]string, 0)
+
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+
+		require.Len(t, result, 0)
+	})
 }
